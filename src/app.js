8c676b04bfd03844badec6d6b4699bb4cbd18875
AlejandraO1 <alejandraortegax@gmail.com>
2024-02-16 13:47:14 -0500

    Improved the User Experience

--- src/app.js
+++ src/app.js
@@ -16,6 +16,10 @@ function generatePoem(event) {
   let context = `You are a romantic poem expert who loves to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Please make sure to follow the user instructions. Do not include a title to the poem. Sign the poem as 'Tu Inteligencia Artificial' in a <strong> element at the end of the poem and not at the beginning.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector(".poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">Generating a Spanish poem about ${instructionsInput.value}...</div>`;
+
   axios.get(apiUrl).then(displayPoem);
 }
 
